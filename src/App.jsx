8b84bcbf8125fe63c61fa7e36f03fd5732d127a1
JLTC3111 <85354982+JLTC3111@users.noreply.github.com>
2025-03-10 00:35:32 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import Grid from "./components/Grid"
-import Hero from "./components/Hero"
-import Layout from "./components/Layout"
-
+import Grid from "./components/Grid";
+import Hero from "./components/Hero";
+import Layout from "./components/Layout";
 
 function App() {
   return (
-    
+    <div style={{ backgroundColor: "white", minHeight: "100vh", color: "black", padding: "20px" }}>
       <Layout>
-      <main>
-        <Hero />
-        <Grid />
-      </main>
-    </Layout>
-  )
+        <main>
+          <Hero />
+          <Grid />
+        </main>
+      </Layout>
+    </div>
+  );
 }
 
-export default App
+export default App;
