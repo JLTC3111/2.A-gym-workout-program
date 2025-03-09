e2c78313f43c646a3164d10cac162ea5cc2d7ce1
JLTC3111 <85354982+JLTC3111@users.noreply.github.com>
2025-03-10 00:27:11 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,9 @@ import Hero from "./components/Hero"
 import Layout from "./components/Layout"
 
 function App() {
-  return (
-    <div style={{ backgroundColor: "white", minHeight: "100vh", color: "black" }}></div>
-  );
 
     return (
-
+    <div style={{ backgroundColor: "white", minHeight: "100vh", color: "black" }}></div>
     <Layout>
       <main>
         <Hero />
